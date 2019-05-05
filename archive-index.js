8879b0c619df67a7c522592eaeedc21c2be1bd65
dani <mayor@dani.town>
2019-05-05 18:32:38 -0400

    magically works

--- archive-index.js
+++ archive-index.js
@@ -1,38 +1,40 @@
 const Web3 = require('web3');
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const abi = require('./abi.js')
+const esmImport = require('esm')(module);
+const { OpenSeaPort, Network } = esmImport('opensea-js');
 
 require('dotenv').config()
 
 const cryptoKittyIds = ['1477497', '1387393']
 const walletAddresses = ['0xAC80f8FefE1F14F40fFf2Bf96210a5B46e0DfD26', '0xE77b8a5a3085AD31eF455F794e46FbBC3AF5cd36']
 
-const web3 = new Web3(new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/62df5323062344249adb11c3403dba29"))
+const provider = new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/62df5323062344249adb11c3403dba29");
+const web3 = new Web3(provider)
+
+const ckContractAddress = '0x06012c8cf97bead5deae237070f9587f8e7a266d'
+
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main
+})
 
 void async function main() {
   // setup
   const accounts = await web3.eth.getAccounts()
   const account = accounts[0]
-  const ckContract = new web3.eth.Contract(abi, '0x06012c8cf97bead5deae237070f9587f8e7a266d', {defaultAccount: account, gas: 400000, gasPrice: 10000000000 })
 
-  // distribute some cats
-  for (let i = 0; i < cryptoKittyIds.length; i++) {
-    console.log(`cat ${cryptoKittyIds[i]}`);
-    console.log(`account: ${account}`);
+  for (let i = 0; i < walletAddresses.length; i++) {
+    if (!cryptoKittyIds[i]) { console.log(`no cryptokitty for ${walletAddresses[i]}`); }
+
+    console.log(`now transferring cryptokitty #${cryptoKittyIds[i]} to ${walletAddresses[i]} from ${account}`);
+
     try {
-      // let approval = await ckContract.methods.approve(walletAddresses[i], cryptoKittyIds[i]).send({
-      //   from: account
-      // })
-      let transfer = await ckContract.methods.transferFrom('0x8d3e809Fbd258083a5Ba004a527159Da535c8abA', walletAddresses[i], cryptoKittyIds[i]).send({
-        from: account
+      const transactionHash = await seaport.transferOne({
+        asset: { tokenId: cryptoKittyIds[i], tokenAddress: ckContractAddress },
+        fromAddress: account,
+        toAddress: walletAddresses[i]
       })
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-
 }()
-
-// gripes:
-// approval succeeds, transfer fails
-// should probably test on rinkeby but how do i get a ck on rinkeby
--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const Web3 = require('web3');
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const esmImport = require('esm')(module);
-const { OpenSeaPort, Network } = esmImport('opensea-js');
+const abi = require('./abi.js')
 
 require('dotenv').config()
 
-const cryptoKittyIds = ['1477497', '1387393']
+const cryptoKittyIds = ['1387393', '1387393']
 const walletAddresses = ['0xAC80f8FefE1F14F40fFf2Bf96210a5B46e0DfD26', '0xE77b8a5a3085AD31eF455F794e46FbBC3AF5cd36']
 
-const provider = new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/62df5323062344249adb11c3403dba29");
-const web3 = new Web3(provider)
-
-const ckContractAddress = '0x06012c8cf97bead5deae237070f9587f8e7a266d'
-
-const seaport = new OpenSeaPort(provider, {
-  networkName: Network.Main
-})
+const web3 = new Web3(new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/62df5323062344249adb11c3403dba29"))
 
 void async function main() {
   // setup
   const accounts = await web3.eth.getAccounts()
   const account = accounts[0]
+  const ckContract = new web3.eth.Contract(abi, '0x06012c8cf97bead5deae237070f9587f8e7a266d', {defaultAccount: account, gas: 400000, gasPrice: 10000000000 })
 
-  for (let i = 0; i < walletAddresses.length; i++) {
-    if (!cryptoKittyIds[i]) { console.log(`no cryptokitty for ${walletAddresses[i]}`); }
-
-    console.log(`now transferring cryptokitty #${cryptoKittyIds[i]} to ${walletAddresses[i]} from ${account}`);
-
+  // distribute some cats
+  for (let i = 0; i < cryptoKittyIds.length; i++) {
+    console.log(`cat ${cryptoKittyIds[i]}`);
+    console.log(`account: ${account}`);
     try {
-      const transactionHash = await seaport.transferOne({
-        asset: { tokenId: cryptoKittyIds[i], tokenAddress: ckContractAddress },
-        fromAddress: account,
-        toAddress: walletAddresses[i]
+      // let approval = await ckContract.methods.approve(walletAddresses[i], cryptoKittyIds[i]).send({
+      //   from: account
+      // })
+      // let transfer = await ckContract.methods.transferFrom('0x8d3e809Fbd258083a5Ba004a527159Da535c8abA', walletAddresses[i], cryptoKittyIds[i]).send({
+      //   from: account
+      // })
+
+      ckContract.methods.transfer(walletAddresses[i], cryptoKittyIds[i]).send({
+        from: account
       })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
+
 }()
+
+// gripes:
+// approval succeeds, transfer fails
+// should probably test on rinkeby but how do i get a ck on rinkeby
